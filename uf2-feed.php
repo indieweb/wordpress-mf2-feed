a6c75ede10f44dfb881abcbf03ce865d7d0963f6
pfefferle <pfefferle@gmail.com>
2015-09-02 09:01:46 +0200

    WordPress PHP Coding Standards

--- uf2-feed.php
+++ uf2-feed.php
@@ -10,144 +10,139 @@ Author URI: http://notizblog.org/
 
 // check if php version is >= 5.3
 // version is required by the mf2 parser
-function uf2_feed_activation()
-{
-    if (version_compare(phpversion(), 5.3, '<')) {
-        die("The minimum PHP version required for this plugin is 5.3");
-    }
+function uf2_feed_activation() {
+	if ( version_compare( phpversion(), 5.3, '<' ) ) {
+		die( 'The minimum PHP version required for this plugin is 5.3' );
+	}
 }
-register_activation_hook(__FILE__, 'uf2_feed_activation');
+register_activation_hook( __FILE__, 'uf2_feed_activation' );
 
-if (!class_exists("Mf2\Parser")) {
-    require_once 'Mf2/Parser.php';
+if ( ! class_exists( 'Mf2\Parser' ) ) {
+	require_once 'Mf2/Parser.php';
 }
 
 use Mf2\Parser;
 
-add_action('init', array('Uf2Feed', 'init'));
-register_activation_hook(__FILE__, array('Uf2Feed', 'flush_rewrite_rules'));
-register_deactivation_hook(__FILE__, array('Uf2Feed', 'flush_rewrite_rules'));
+add_action( 'init', array( 'Uf2Feed', 'init' ) );
+register_activation_hook( __FILE__, array( 'Uf2Feed', 'flush_rewrite_rules' ) );
+register_deactivation_hook( __FILE__, array( 'Uf2Feed', 'flush_rewrite_rules' ) );
 
 /**
+ * Uf2Feed class
  *
+ * @author Matthias Pfefferle
  */
-class Uf2Feed
-{
-    /**
-     * init function
-     */
-    public static function init()
-    {
-        // add 'json' as feed
-        add_action('do_feed_uf2', array('Uf2Feed', 'do_feed_uf2'), 10, 1);
-        // add 'json' as feed
-        add_action('do_feed_mf2', array('Uf2Feed', 'do_feed_uf2'), 10, 1);
-        // add the as1 feed
-        add_feed('uf2', array('Uf2Feed', 'do_feed_uf2'));
-        // add the as2 feed
-        add_feed('mf2', array('Uf2Feed', 'do_feed_uf2'));
+class Uf2Feed {
+	/**
+	 * init function
+	 */
+	public static function init() {
+		// add 'json' as feed
+		add_action( 'do_feed_uf2', array( 'Uf2Feed', 'do_feed_uf2' ), 10, 1 );
+		// add 'json' as feed
+		add_action( 'do_feed_mf2', array( 'Uf2Feed', 'do_feed_uf2' ), 10, 1 );
+		// add the as1 feed
+		add_feed( 'uf2', array( 'Uf2Feed', 'do_feed_uf2' ) );
+		// add the as2 feed
+		add_feed( 'mf2', array( 'Uf2Feed', 'do_feed_uf2' ) );
 
-        add_filter('query_vars', array('Uf2Feed', 'query_vars'));
-        add_filter('feed_content_type', array('Uf2Feed', 'feed_content_type'), 10, 2);
-    }
+		add_filter( 'query_vars', array( 'Uf2Feed', 'query_vars' ) );
+		add_filter( 'feed_content_type', array( 'Uf2Feed', 'feed_content_type' ), 10, 2 );
+	}
 
-    /**
-     * adds an UF2 JSON feed
-     *
-     * @param boolean $for_comments true if it is a comment-feed
-     */
-    public static function do_feed_uf2($for_comments)
-    {
-        global $wp;
+	/**
+	 * adds an UF2 JSON feed
+	 *
+	 * @param boolean $for_comments true if it is a comment-feed
+	 */
+	public static function do_feed_uf2( $for_comments ) {
+		global $wp;
 
-        // get query vars as array
-        $params = $wp->query_vars;
+		// get query vars as array
+		$params = $wp->query_vars;
 
-        // remove feed param
-        if (isset($params["feed"])) {
-            unset($params["feed"]);
-        }
+		// remove feed param
+		if ( isset( $params['feed'] ) ) {
+			unset( $params['feed'] );
+		}
 
-        $current_url = add_query_arg($params, site_url());
+		$current_url = add_query_arg( $params, site_url() );
 
-        // filter feed URL (add url params for example)
-        $current_url = apply_filters("uf2_feed_url", $current_url);
+		// filter feed URL (add url params for example)
+		$current_url = apply_filters( 'uf2_feed_url', $current_url );
 
-        // get HTML content
-        $response = wp_remote_retrieve_body(wp_remote_get($current_url, array('timeout' => 100)));
+		// get HTML content
+		$response = wp_remote_retrieve_body( wp_remote_get( $current_url, array( 'timeout' => 100 ) ) );
 
-        // parse source html
-        $parser = new Parser($response, $current_url);
+		// parse source html
+		$parser = new Parser( $response, $current_url );
 
-        // also support uf1?
-        $parseUf1 = apply_filters("uf2_feed_support_uf1", false);
+		// also support uf1?
+		$parseUf1 = apply_filters( 'uf2_feed_support_uf1', false );
 
-        $mf_array = $parser->parse($parseUf1);
+		$mf_array = $parser->parse( $parseUf1 );
 
-        // filter output
-        $json = apply_filters("uf2_feed_array", $mf_array);
+		// filter output
+		$json = apply_filters( 'uf2_feed_array', $mf_array );
 
-        header("Content-Type: " . feed_content_type("uf2") . "; charset=" . get_option("blog_charset"), true);
+		header( 'Content-Type: ' . feed_content_type( 'uf2' ) . '; charset=' . get_option( 'blog_charset' ), true );
 
-        if (version_compare(phpversion(), "5.3.0", "<")) {
-            // json_encode() options added in PHP 5.3
-            $json_str = json_encode($json);
-        } else {
-            $options = 0;
-            // JSON_PRETTY_PRINT added in PHP 5.4
-            if (get_query_var("pretty") && version_compare(phpversion(), "5.4.0", ">=")) {
-                $options |= JSON_PRETTY_PRINT;
-            }
+		if ( version_compare( phpversion(), '5.3.0', '<' ) ) {
+			// json_encode() options added in PHP 5.3
+			$json_str = json_encode( $json );
+		} else {
+			$options = 0;
+			// JSON_PRETTY_PRINT added in PHP 5.4
+			if ( get_query_var( 'pretty' ) && version_compare( phpversion(), '5.4.0', '>=' ) ) {
+				$options |= JSON_PRETTY_PRINT;
+			}
 
-            /*
-             * Options to be passed to json_encode()
-             *
-             * @param int $options The current options flags
-             */
-            $options = apply_filters("uf2_feed_options", $options);
-            $json_str = json_encode($json, $options);
-        }
+			/*
+			 * Options to be passed to json_encode()
+			 *
+			 * @param int $options The current options flags
+			 */
+			$options = apply_filters( 'uf2_feed_options', $options );
+			$json_str = json_encode( $json, $options );
+		}
 
-        echo $json_str;
-    }
+		echo $json_str;
+	}
 
-    /**
-     * adds "uf2" content-type
-     *
-     * @param string $content_type the default content-type
-     * @param string $type the feed-type
-     * @return string the as1 content-type
-     */
-    public static function feed_content_type($content_type, $type)
-    {
-        if ($type == "uf2" || $type == "mf2") {
-            return apply_filters("uf2_feed_content_type", "application/json");
-        }
+	/**
+	 * adds "uf2" content-type
+	 *
+	 * @param string $content_type the default content-type
+	 * @param string $type the feed-type
+	 * @return string the as1 content-type
+	 */
+	public static function feed_content_type( $content_type, $type ) {
+		if ( 'uf2' == $type || 'mf2' == $type ) {
+			return apply_filters( 'uf2_feed_content_type', 'application/json' );
+		}
 
-        return $content_type;
-    }
+		return $content_type;
+	}
 
-    /**
-     * add 'feed' and 'pretty' as a valid query variables.
-     *
-     * @param array $vars
-     * @return array
-     */
-    public static function query_vars($vars)
-    {
-        $vars[] = 'feed';
-        $vars[] = 'pretty';
+	/**
+	 * add 'feed' and 'pretty' as a valid query variables.
+	 *
+	 * @param array $vars
+	 * @return array
+	 */
+	public static function query_vars( $vars ) {
+		$vars[] = 'feed';
+		$vars[] = 'pretty';
 
-        return $vars;
-    }
+		return $vars;
+	}
 
-    /**
-     * reset rewrite rules
-     */
-    public static function flush_rewrite_rules()
-    {
-        global $wp_rewrite;
+	/**
+	 * reset rewrite rules
+	 */
+	public static function flush_rewrite_rules() {
+		global $wp_rewrite;
 
-        $wp_rewrite->flush_rules();
-    }
+		$wp_rewrite->flush_rules();
+	}
 }

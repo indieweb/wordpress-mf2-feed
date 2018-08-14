15c6955f1905886fa539ff4e593d151b8d41d574
Matthias Pfefferle <pfefferle@gmail.com>
2018-08-14 09:09:46 +0200

    fixed c&p issue

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/indieauth.php';
+	require dirname( dirname( __FILE__ ) ) . '/mf2-feed.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

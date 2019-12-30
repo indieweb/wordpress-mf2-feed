c28e99b14f8d40744a75c1bfd57823f3174ea38e
David Shanske <david@shanske.com>
2019-12-30 03:26:07 +0000

    Fix missing information at top-level in jf2feed.

--- includes/feed-jf2.php
+++ includes/feed-jf2.php
@@ -9,7 +9,16 @@ header( 'Content-Type: ' . feed_content_type( 'jf2feed' ), true );
 
 require_once dirname( __FILE__ ) . '/class-mf2-feed-entry.php';
 
-$items = array( 'type' => 'feed' );
+$items = array(
+	'type'    => 'feed',
+	'name'    => get_bloginfo( 'name' ),
+	'summary' => get_bloginfo( 'description' ),
+	'url'     => get_self_link(),
+);
+if ( ! empty( $featured ) ) {
+	$items['featured'] = $featured;
+}
+
 while ( have_posts() ) {
 	the_post();
 	$item                = new Mf2_Feed_Entry( get_the_ID() );
